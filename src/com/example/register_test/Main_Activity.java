72ebf8c6658866696865109392f4095ab5e63765
David Coronado Dimalanta <dimalantadavid@hotmail.com>
2012-07-24 17:10:37 +0800

    I had eaten my first Big n' Tasty at McDonald's last night.

---
+++ src/com/example/register_test/Main_Activity.java
@@ -0,0 +1,24 @@
+package com.example.register_test;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class Main_Activity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    
+}
